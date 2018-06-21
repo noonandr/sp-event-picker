d9b8cd7c78c2fe283150de98933bed7a4cbe440b
Daniel Noonan <noonan_dr@yahoo.co.uk>
2018-06-21 01:02:43 +0100

    feat(vue): #SP-002 basic setup of Vue with picker component init and image icon assests for dropdown

---
+++ src/components/EventPicker.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Pick me
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EventPicker',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <EventPicker/>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+import EventPicker from '@/components/EventPicker.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    EventPicker
   }
 }
 </script>
